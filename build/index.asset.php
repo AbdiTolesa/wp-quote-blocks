2d1e0bd24fa71d45e043692d2eefef943b9f27df
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2026-04-27 19:04:16 +0300

    Update block title

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0dc7959a28bffe0b6b8a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0afcb5f9d80bc2c8c6b8');
